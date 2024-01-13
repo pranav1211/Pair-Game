d9ef5ec3c96e215891215673e3f611103cd347bb
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-13 11:32:09 +0000

    1/13/2024

--- pairgame.py
+++ pairgame.py
@@ -212,6 +212,10 @@ cellfiller2 = Button(root,text="",
                 bg="green",)
 cellfiller2.pack()
 
+cellfiller2 = Button(root,text="",
+                font=("Arial",25),
+                bg="green",)
+cellfiller2.pack()
 ########################################################
 
 cell9 = Button(root,text="9",
