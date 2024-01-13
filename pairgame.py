03e84b0cfc2f4aa31a27a37b72b64c7bdea33480
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-13 11:55:05 +0000

    1/13/2024

--- pairgame.py
+++ pairgame.py
@@ -63,20 +63,7 @@ def startgame():
     frame2.pack(side=TOP, padx=5, pady=5)
     frame3.pack(side=TOP, padx=5, pady=5)
     
-    cell1.pack(frame1,side=LEFT)
-    cell2.pack(frame1,side=LEFT)
-    cell3.pack(frame1,side=LEFT)
-    cell4.pack(frame1,side=LEFT)
-       
-    cell5.pack(frame2,side=LEFT)
-    cell6.pack(frame2,side=LEFT)
-    cell7.pack(frame2,side=LEFT)
-    cell8.pack(frame2,side=LEFT)
-       
-    cell9.pack(frame3,side=LEFT)
-    cell10.pack(frame3,side=LEFT)
-    cell11.pack(frame3,side=LEFT)
-    cell12.pack(frame3,side=LEFT)
+    
     
     
 
@@ -159,25 +146,25 @@ cell1 = Button(root,text="1",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell1.pack()
+
 
 cell2 = Button(root,text="2",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell2.pack()
+
 
 cell3 = Button(root,text="3",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell3.pack()
+
 
 cell4 = Button(root,text="4",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell4.pack()
+
 
 #############################################################
 
@@ -185,25 +172,25 @@ cell5 = Button(root,text="5",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell5.pack()
+
 
 cell6 = Button(root,text="6",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell6.pack()
+
 
 cell7 = Button(root,text="7",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell7.pack()
+
 
 cell8 = Button(root,text="8",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell8.pack()
+
 
 ########################################################
 
@@ -211,27 +198,39 @@ cell9 = Button(root,text="9",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell9.pack()
+
 
 cell10 = Button(root,text="10",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell10.pack()
+
 
 cell11 = Button(root,text="11",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell1.pack()
+
 
 cell12 = Button(root,text="12",
               font=("Arial",25),
                bg="black",
                fg='white',)
-cell12.pack()
 
-
+cell1.pack(frame1,side=LEFT)
+cell2.pack(frame1,side=LEFT)
+cell3.pack(frame1,side=LEFT)
+cell4.pack(frame1,side=LEFT)
+      
+cell5.pack(frame2,side=LEFT)
+cell6.pack(frame2,side=LEFT)
+cell7.pack(frame2,side=LEFT)
+cell8.pack(frame2,side=LEFT)
+       
+cell9.pack(frame3,side=LEFT)
+cell10.pack(frame3,side=LEFT)
+cell11.pack(frame3,side=LEFT)
+cell12.pack(frame3,side=LEFT)
 
 
 
