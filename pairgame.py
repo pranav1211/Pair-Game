63457d5d10873b9672020def870b733e0033d773
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-13 11:27:36 +0000

    1/13/2024

--- pairgame.py
+++ pairgame.py
@@ -70,7 +70,7 @@ def startgame():
     cell3.pack(side=LEFT)
     cell4.pack(side=LEFT)
     
-    cellfiller1.pack(side=LEFT)
+    cellfiller1.pack()
     
     cell5.pack(side=LEFT)
     cell6.pack(side=LEFT)
